fab8c48abc71ac180b348cfee889e7c1da2f09ce
viktoria.klisheva <vikmah2@gmail.com>
2022-01-09 18:55:38 +0300

    add

---
+++ src/test/java/guru/qa/PracticForm.java
@@ -0,0 +1,67 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticForm {
+    @Test
+    void practicForm() {
+        open("https://demoqa.com/automation-practice-form");
+        //заполняеи поле имя
+        $("[id=firstName]").setValue("Vitoriia");
+
+        //заполняеи поле фамилия
+        $("[id=lastName]").setValue("Sun");
+
+        // заполняем эл.почту
+        $("#userEmail").setValue("IronLady@gmail.com");
+
+        //выбираем пол
+        $("[for=gender-radio-1]").click();
+
+        //заполняем тел
+        $("[id=userNumber]").setValue("9302764785");
+
+        //открываем календарь
+        $("#dateOfBirthInput").click();
+        //выбираем месяц
+        $(".react-datepicker__month-select").selectOptionByValue("4");
+        //выбираем год
+        $(".react-datepicker__year-select").selectOptionByValue("1993");
+        //выбираем рандомную дату
+        $(".react-datepicker__day").click();
+
+        //скролл страницы вниз
+        $(byText("Sports")).scrollTo();
+
+        //кликаем на поле с выпадающим списком
+        $("#subjectsInput").click();
+        //вбиваем букву ,чтобы появился выпадающий список
+        $("#subjectsInput").setValue("d");
+        //выбираем значение из списка
+        $(byText("Hindi")).click();
+
+        //заполняем увлечения
+        $("[for=hobbies-checkbox-1]").click();
+
+        //загрузка файла
+        $("#uploadPicture").uploadFile(new File("src/test/resources/cv.png"));
+
+        $("[id=currentAddress]").setValue("Moscow Kremlin"); //пишем адрес
+        $("#state").click();
+        $(byText("NCR")).click();   //выбираем значение из списка Штат
+        $("#city").click();
+        $(byText("Noida")).click();   //выбираем значение из списка город
+        $("[id=submit]").click();   //кликаем на кнопку Submit
+
+
+    }
+
+
+}
+
